ce1af430fd366e493e72cc8c635b08c7a92347e5
Carlingr <kittyclubco@gmail.com>
2017-01-22 11:18:48 -0500

    made playable game
    Still needs some work, but a good beta

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ var weight
 var pathSeg
 var lines = []
 var colors = []
-var gameState = 0; //0 is play, 1 is loss
+var gameState = 0; //0 is play, 1 is loss, 2 is win
 var cnv
 
 function preload() {
@@ -16,32 +16,19 @@ function preload() {
 
 function setup() {
   cnv = createCanvas(windowWidth, windowHeight);
-  //angleMode(DEGREES) //make my head hurt lessship = createSprite(width/2, height/2);
 
   ship.friction = .93
   ship.addImage(shipImage)
 
-  colors[0] = color("blue")
-  colors[1] = color("aqua")
-  colors[2] = color("#0e0");
+  colors[0] = color('blue');
+  colors[1] = color('aqua');
+  colors[2] = color('#0e0');
 
   generateGame(100, 50);
+
 }
 
 function draw() {
-  end = millis();
-  var rot = (sqrt((ship.velocity.y * ship.velocity.y) + (ship.velocity.x * ship.velocity.x)))
-
-  if (keyDown("A")) {
-    ship.rotation -= rot * 2;
-  }
-  if (keyDown("D")) {
-    ship.rotation += rot * 2;
-  }
-  if (keyDown("W")) {
-    ship.addSpeed(.2, ship.rotation);
-  }
-
   background(colors[2]);
 
   stroke(colors[1]);
@@ -53,34 +40,73 @@ function draw() {
     lines[i].disp2();
   }
 
-  if (dist(ship.position.x, ship.position.y, lines[lines.length - 1].x2, lines[lines.length - 1].y2) < 15) {
-    generateGame(90, 50)
+  if (dist(ship.position.x, ship.position.y, lines[lines.length - 1].x2, lines[lines.length - 1].y2) < 50) {
+    gameState = 2;
   }
 
-  if (cnv.get(ship.position.x, ship.position.y) == colors[0]) {}
+  noStroke();
 
-  if (gameState == 1) { //if the game is over
-    background("green");
-    console.log("I think you found the shore!")
+  textAlign(LEFT, TOP);
+  fill("white");
+  textStyle(BOLD);
+  textSize(20)
+  text("WAD to move", 20, 20)
+  text(round((end - start) / 10) / 100, 20, 40)
+
+  if (gameState === 0) {
+    end = millis();
+    var rot = (sqrt((ship.velocity.y * ship.velocity.y) + (ship.velocity.x * ship.velocity.x)))
+
+    if (keyDown("A")) {
+      ship.rotation -= rot * 2;
+    }
+    if (keyDown("D")) {
+      ship.rotation += rot * 2;
+    }
+    if (keyDown("W")) {
+      ship.addSpeed(.2, ship.rotation);
+    }
+
+    var shipColor = color(cnv.get(ship.position.x, ship.position.y));
+
+    switch (shipColor.levels[1]) {
+      case colors[0].levels[1]:
+        ship.friction = .93;
+        break;
+      case colors[1].levels[1]:
+        ship.friction = .9
+        break;
+      case colors[2].levels[1]:
+        gameState = 1;
+        break;
+      default:
+    }
+  } else if (gameState == 1) { //if the game is over
+    textAlign(CENTER, CENTER);
+    fill("white");
+    textStyle(BOLD);
+    textSize(width / 50)
+    text("You've run aground! Reload the page to play again", width / 2, height / 2)
+  } else if (gameState == 2) {
+    textAlign(CENTER, CENTER);
+    fill("white");
+    textStyle(BOLD);
+    textSize(width / 50)
+    text("You've reached the end of the course! Reload the page to play again", width / 2, height / 2)
   }
-
   drawSprites();
-}
-
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight)
 }
 
 function generateGame(level, strk) {
   var pathX = []
-  var pathY = []
-  for (var i = 0; i < (height / level) + 1; i++) {
-    pathY.push(round(random(i * level, i * level)))
+  var pathY = [];
+  lines = []
+  for (var i = 0; i < (height / (level * 1.50)); i++) {
+    pathY.push(round(random(i * (level * 1.50), i * (level * 1.50))))
     pathX.push(round(random(0, width)))
   }
   pathX.push(round(random(0, width)))
-  for (i = 0; i < pathY.length; i += 1) {
+  for (i = 0; i < pathY.length - 1; i += 1) {
     lines.push(new Line(pathX[i], pathY[i], pathX[i], pathY[i + 1], strk))
     lines.push(new Line(pathX[i], pathY[i + 1], pathX[i + 1], pathY[i + 1], strk))
   }
