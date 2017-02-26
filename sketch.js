316a2559734cb765f2c88457fe8d389702c03409
Carlingr <kittyclubco@gmail.com>
2017-02-26 13:09:23 -0500

    update to new style and script locations

--- sketch.js
+++ sketch.js
@@ -1,114 +1,128 @@
-var ship
-var shpImg
-var start
-var end
-var weight
-var pathSeg
-var lines = []
-var colors = []
+var ship //holds the ship object
+var shpImg //holds the image used for the ship
+var start //holds start time
+var end //holds endtime
+var lines = [] //holds the path the boat needs to take
+var colors = [] //holds the colors used, this is so they can be refrenced later
 var gameState = 0; //0 is play, 1 is loss, 2 is win
-var cnv
+var cnv //holds the canvas in case of DOM
 
 function preload() {
-  shipImage = loadImage("images/ship.svg");
-  ship = createSprite();
+  shipImage = loadImage("images/ship.svg"); //load the image for the ship
+  ship = createSprite(); //make sprite in p5.play
 }
 
 function setup() {
-  cnv = createCanvas(windowWidth, windowHeight);
+  var wth; //holds width of canvas
+  if (windowWidth < 500) { //the game is too hard to play if the canvas is giant
+    wth = windowWidth //set the width to the window width
+  } else { // if the window is biiger then 500
+    wth = 500; //the the width to 500
+  }
+  if (windowHeight < 500) { //the game is too hard to play if the canvas is giant
+    cnv = createCanvas(wth, windowHeight); //make a small canvas
+  } else { // if the window is biiger then 500
+    cnv = createCanvas(wth, 500) //make a big canvas
+  }
 
-  ship.friction = .93
-  ship.addImage(shipImage)
-
+  ship.friction = .93; //put some friction in the situation
+  ship.addImage(shipImage); //put a face to the name
+  //<init the colors>
   colors[0] = color('blue');
   colors[1] = color('aqua');
   colors[2] = color('#0e0');
-
-  generateGame(100, 50);
-
+  //</init the colors>
+  generateGame(); //make the game
 }
 
 function draw() {
-  background(colors[2]);
-
-  stroke(colors[1]);
-  for (i = 0; i < lines.length; i++) {
-    lines[i].disp1();
+  background(colors[2]); //make a background
+  //<draw the lines>
+  stroke(colors[1]); //this is the color of the outer lines
+  for (i = 0; i < lines.length; i++) { //loop through all the things
+    lines[i].disp1(); //draw the first part of the line
   }
-  stroke(colors[0])
-  for (i = 0; i < lines.length; i++) {
-    lines[i].disp2();
+  stroke(colors[0]) //color of the inner lines
+  for (i = 0; i < lines.length; i++) { //around again
+    lines[i].disp2(); //the inner lines this time
+  }
+  //</draw the lines>
+  if (dist(ship.position.x, ship.position.y, lines[lines.length - 1].x2, lines[lines.length - 1].y2) < 50) { //if we finish the course
+    gameState = 2; //goto Win
   }
 
-  if (dist(ship.position.x, ship.position.y, lines[lines.length - 1].x2, lines[lines.length - 1].y2) < 50) {
-    gameState = 2;
-  }
-
-  noStroke();
-
+  noStroke(); //ditch the stroke
+  //<make the text work>
   textAlign(LEFT, TOP);
   fill("white");
   textStyle(BOLD);
   textSize(20)
-  text("WAD to move", 20, 20)
-  text(round((end - start) / 10) / 100, 20, 40)
+    //</make the text work>
+  text("WAD to move", 20, 20) //Instructions in the corner
+  text(round((end - start) / 10) / 100, 20, 40) //time on course
 
-  if (gameState === 0) {
-    end = millis();
-    var rot = (sqrt((ship.velocity.y * ship.velocity.y) + (ship.velocity.x * ship.velocity.x)))
+  if (gameState === 0) { //if we re playing
+    end = millis(); //note the time
+    var rot = (sqrt((ship.velocity.y * ship.velocity.y) + (ship.velocity.x * ship.velocity.x))); //sort out how fast were going
+    //p5.play helpfully only give the x and y velocities seperately, so we have to use pytagorus to sort it
+    //rot makes a numerical relationshp betwwen the forward speed and the turnign speed
 
-    if (keyDown("A")) {
-      ship.rotation -= rot * 2;
+    if (keyDown("A")) { //if A key is pressed
+      ship.rotation -= rot * 2; //turn left
     }
-    if (keyDown("D")) {
-      ship.rotation += rot * 2;
+    if (keyDown("D")) { //if D key is pressed
+      ship.rotation += rot * 2; //turn right
     }
-    if (keyDown("W")) {
-      ship.addSpeed(.2, ship.rotation);
+    if (keyDown("W")) { //if W key is pressed
+      ship.addSpeed(.2, ship.rotation); //give her power
     }
 
-    var shipColor = color(cnv.get(ship.position.x, ship.position.y));
+    var shipColor = color(cnv.get(ship.position.x, ship.position.y)); //gets the color of the canvas under the ship to determine how close to shore it is
 
-    switch (shipColor.levels[1]) {
-      case colors[0].levels[1]:
-        ship.friction = .93;
+    switch (shipColor.levels[1]) { //google switch/case
+      case colors[0].levels[1]: //if in the main channel
+        ship.friction = .93; //full speed
         break;
-      case colors[1].levels[1]:
-        ship.friction = .9
+      case colors[1].levels[1]: //if approacing shore
+        ship.friction = .9 //slow down
         break;
-      case colors[2].levels[1]:
-        gameState = 1;
+      case colors[2].levels[1]: //if aground
+        gameState = 1; //end game
         break;
       default:
     }
-  } else if (gameState == 1) { //if the game is over
+  } else if (gameState == 1) { //if we lost
+    //<let the player know>
     textAlign(CENTER, CENTER);
     fill("white");
     textStyle(BOLD);
     textSize(width / 50)
-    text("You've run aground! Reload the page to play again", width / 2, height / 2)
-  } else if (gameState == 2) {
+    text("You've run aground! Reload the page to play again", width / 2, height / 2);
+    //</let the player know>
+  } else if (gameState == 2) { //if we won
+    //<let the player know>
     textAlign(CENTER, CENTER);
     fill("white");
     textStyle(BOLD);
     textSize(width / 50)
-    text("You've reached the end of the course! Reload the page to play again", width / 2, height / 2)
+    text("You've reached the end of the course! Reload the page to play again", width / 2, height / 2);
+    //<let the player know>
   }
-  drawSprites();
+  drawSprites(); //fnd the boat
 }
 
-function generateGame(level, strk) {
-  var pathX = []
+function generateGame() { //make a new game
+  var pathX = []; //
   var pathY = [];
   lines = []
-  for (var i = 0; i < (height / (level * 1.50)); i++) {
-    pathY.push(round(random(i * (level * 1.50), i * (level * 1.50))))
+  for (var i = 0; i < (height / (100 * 1.50)); i++) {
+    pathY.push(round(random(i * (100 * 1.50), i * (100 * 1.50))))
     pathX.push(round(random(0, width)))
   }
   pathX.push(round(random(0, width)))
   for (i = 0; i < pathY.length - 1; i += 1) {
-    lines.push(new Line(pathX[i], pathY[i], pathX[i], pathY[i + 1], strk))
-    lines.push(new Line(pathX[i], pathY[i + 1], pathX[i + 1], pathY[i + 1], strk))
+    lines.push(new Line(pathX[i], pathY[i], pathX[i], pathY[i + 1], 50))
+    lines.push(new Line(pathX[i], pathY[i + 1], pathX[i + 1], pathY[i + 1], 50))
   }
   ship.rotation = 90
   ship.position.x = lines[0].x1
